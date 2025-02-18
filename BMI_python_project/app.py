0ab5626f35b820e6ea37c293cff54cf82ce6c89d
SJcodeML <sidrajabin56@gmail.com>
2025-02-18 22:50:04 +0500

    Data _analysation_app added

--- BMI_python_project/app.py
+++ BMI_python_project/app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pandas
+
+
 st.title("BMI Calculator")
 
 height = st.slider("Enter your height (in cm):", 100,250,175 )
-weight = st.slider("Enter your weight (in kg)", 40, 200, 70)
+weight = st.slider("Enter your weight (in kg)", 20, 200, 70)
 
-bmi weight / ((height/100) ** 2)
+bmi= weight / ((height/100) ** 2)
 
 st.write(f"Your BMI is {bmi:.2f}")
+
+st.write("Underweight: BMI less than 18.5")
+st.write("Normal weight: BMI 18.5 to 24.9")
+st.write("Overweight: BMI 25 to 29.9")
+st.write("Obese: BMI 30 or greater")
+
 
---
+++ Data_Analysation_App/data.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+st.title("Simple data Dashboard")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+st.write("File Uploaded")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    
+    st.subheader("Data Preview")
+    st.write(df.head())
+   
+    st.subheader("Data Summary")
+    st.write(df.describe())
+   
+    st.subheader("Filter Data")
+    columns = df.columns.tolist()
+    selected_column = st.selectbox("Select column to filter by:" ,columns)
+    unique_values = df[selected_column].unique()
+    selected_value =  st.selectbox("Select Value", unique_values)
+
+    filtered_df = df[df[selected_column] == selected_value]
+    st.write(filtered_df)
+
+    st.subheader("Plot Data")
+    x_coloumn = st.selectbox("Select column for x-axis", columns)
+    y_coloumn = st.selectbox("Select column for y-axis", columns)
+   
+    if st.button("Generate Plot"):
+       st.line_chart(filtered_df.set_index(x_coloumn)[y_coloumn])
+
+st.write("Waiting for File Upload")       
+   
+
+
+
+
