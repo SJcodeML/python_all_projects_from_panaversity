c09dc64c618c79bdbda9ffce0f5bc1952364b288
SJcodeML <sidrajabin56@gmail.com>
2025-04-23 22:50:14 +0500

    assignment added

---
+++ 4-assignment/assignments-01-advance/8-data-driven/data-driven.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+st.set_page_config(page_title = "Thread App" , layout= "wide" , initial_sidebar_state="expanded")
+
+st.title("Themed streamlit App")
+st.write("This app has a customized theme!")
+
+
